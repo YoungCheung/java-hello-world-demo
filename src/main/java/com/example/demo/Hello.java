ef12fcdae71adc09707d6c4c8df412d1897d7a18
zhangyang <18813001922@139.com>
2019-08-23 13:28:18 +0800

    hello word

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Created by qcl on 2019-06-05
+ * desc:
+ */
+@RestController
+public class Hello {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
