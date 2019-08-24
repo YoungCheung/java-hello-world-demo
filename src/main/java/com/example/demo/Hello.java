9096dac476466101b1eb4624d0599ae279eb1778
zhangyang <s.c.young@hotmail.com>
2019-08-24 10:44:06 +0800

    Update Hello.java

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("/")
     public String hello() {
-        return "Hello World!---update---";
+        return "Hello World!";
     }
 }
