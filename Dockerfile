09a0aa59a09cc8dbc91125f9661b646ac4e3ce5a
zhangyangwx <18813001922@139.om>
2019-08-25 16:18:56 +0800

    add build docker images

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM harbor.umsauto.cn/devops/jar-java-demo:latest
-MAINTAINER "zhsir"
-ADD default /data/webapps/
-WORKDIR /data/webapps/
-EXPOSE 8080
-ENTRYPOINT ["./run.sh"]
--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+
 /**
  * Created by qcl on 2019-06-05
  * desc:
@@ -12,4 +14,9 @@ public class Hello {
     public String hello() {
         return "Hello World!";
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
+
